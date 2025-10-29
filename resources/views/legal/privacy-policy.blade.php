6532ff5076246d7a5245338dd88514f229c42102
vitormvrdev <vitormvr2001@gmail.com>
2025-10-29 19:30:01 +0000

    Erasing test commit contents

--- resources/views/legal/privacy-policy.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Privacy Policy</h1>
-    <p>Your privacy is important to us. This privacy policy explains how we collect, use, and protect your information.</p>
-@endsection
